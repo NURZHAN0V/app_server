80b9321157d9ef80389d1e10e57a43e57eba5ee0
gitlisso <lisso@nastyle.pro>
2025-08-19 16:25:01 +0300

    Express.js. Первый сервер, маршруты

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Привет, мир!')
-})
+  res.send('Привет, мир! Сервер обновился!')
+});
+
+app.get('/info', (req, res) => {
+  res.json({
+    server: 'App Server',
+    version: '0.0.2',
+    status: 'работает'
+  });
+});
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ message: `Привет, ${name}!` });
+});
 
 app.listen(port, () => {
   console.log(`Приложение запущено и работает на порту ${port}`)
