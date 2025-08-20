7b3bd06d98ae56ca1bdacface1a62f557f826630
gitlisso <lisso@nastyle.pro>
2025-08-20 15:11:58 +0300

    Знакомства и настройка базы данных SQLite

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const db = require('./config/db');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Привет, мир! Сервер обновился!')
@@ -19,6 +23,41 @@ app.get('/hello/:name', (req, res) => {
   res.json({ message: `Привет, ${name}!` });
 });
 
+// Задачи CRUD
+app.post('/tasks', (req, res) => {
+  const { title } = req.body;
+
+  if (!title) return res.status(400).json({ error: 'Поле title пустое' })
+
+  const sql = 'INSERT INTO tasks (title) VALUES (?)';
+  db.run(sql, [title], (err) => {
+    if (err) return res.status(500).json({ error: 'Не удалось создать задачу' });
+
+    res.status(201).json({
+      message: 'Задача успешно создана',
+      title: title
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log(`Приложение запущено и работает на порту ${port}`)
-})
+});
+
+
+// закрытие базы данных по завершению работы сервера
+const shutdown = () => {
+  console.log("Останавливаем сервер...");
+
+  db.close(err => {
+    if (err) {
+      console.error('Не получилось закрыть базу');
+      process.exit(1);
+    }
+    console.log('База закрыта. До встречи!');
+    process.exit(0);
+  })
+}
+
+process.on('SIGINT', shutdown)
+process.off('SIGTERM', shutdown)
